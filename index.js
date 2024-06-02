b723340033a2b227d75243488261b5e728090e0e
usama3105 <um354626@gmail.com>
2024-06-02 18:18:15 +0500

    word counter code

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+async function main() {
+    const userInput = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter your text:',
+            validate: (input) => input.trim().length > 0,
+        },
+    ]);
+    const wordCount = userInput.text.trim().split(/\s+/).length;
+    console.log(`Word count: ${wordCount}`);
+}
+main();
